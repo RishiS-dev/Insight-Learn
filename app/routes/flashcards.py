733c3ca2538bf8b0922969633b34d059465d8a8c
Rishi Sundaresan <rishi200322@gmail.com>
2025-09-25 12:12:27 +0530

    get flashcards
    retrieve flashcards from the db

--- app/routes/flashcards.py
+++ app/routes/flashcards.py
@@ -39,3 +39,28 @@ def create_flashcards(doc_id: int, user: dict = Depends(get_current_user), num_c
         "flashcard_ids": ids,
         "flashcards": flashcards
     }
+@router.get("/list/{doc_id}")
+def get_flashcards(doc_id: int, user: dict = Depends(get_current_user)):
+    con = get_db_connection()
+    cur = con.cursor()
+
+    cur.execute("SELECT id FROM documents WHERE id = %s AND user_id = %s",(doc_id, user["id"]))
+    doc = cur.fetchone()
+    if not doc:
+        raise HTTPException(status_code=404, detail="Document not found")
+    
+    cur.execute("SELECT id, question, answer, created_at FROM flashcards WHERE document_id = %s ORDER BY created_at DESC", (doc_id,))
+    rows = cur.fetchall()
+    cur.close()
+    con.close()
+
+    flashcards = [
+        {"id": row[0], "question": row[1], "answer": row[2], "created_at": row[3]}
+        for row in rows
+    ]
+
+    return{
+        "document_id": doc_id,
+        "flashcards": flashcards,
+        "total_flashcards": len(flashcards)
+    }
